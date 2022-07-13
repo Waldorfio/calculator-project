f8cb946ff6982759e6c790bae990e4366e966c73
Waldorfio <walid.hanifi707@gmail.com>
2022-07-13 21:23:07 +1000

    Add basic math functions

--- code.js
+++ code.js
@@ -1,3 +1,20 @@
 function add(a, b) {
-    return (a + b)
+    ans = a + b;
+    return ans
+}
+function subtract(a, b) {
+    ans = a - b;
+    return ans
+}
+function multiply(a, b) {
+    ans = a * b;
+    return ans
+}
+function divide(a, b) {
+    ans = a / b;
+    return ans
+}
+function percent(a, b) {
+    ans = a/100;
+    return ans;
 }
