f450e528f7f15adbf34498b7c083b36debec3a01
Waldorfio <walid.hanifi707@gmail.com>
2022-07-14 18:35:02 +1000

    Add working display and twice add

--- code.js
+++ code.js
@@ -1,29 +1,53 @@
-function add(a, b) {
-    ans = a + b;
-    return ans
-}
-function subtract(a, b) {
-    ans = a - b;
-    return ans
-}
-function multiply(a, b) {
-    ans = a * b;
-    return ans
-}
-function divide(a, b) {
-    ans = a / b;
-    return ans
-}
-function percent(a, b) {
-    ans = a/100;
-    return ans;
+function storeNumber(number, numArr) {
+    noClicked = number.innerHTML;
+    numArr.push(parseInt(noClicked));
+    display.innerHTML = numArr.join('');    // Display numArr without any commas
+    return numArr
 }
 
-function storeNumber(number) {
-    outputNo = number.innerHTML;
-    alert(outputNo);
+let numArr = [];
+display = document.getElementById('display');
+
+for (i=1; i<9+1; i++) {;
+  number = document.getElementById('number'+i);
+  number.addEventListener('click', storeNumber.bind(this,number,numArr));
 }
 
-number = document.getElementsByClassName(".number");
-number.addEventListener('click', () => storeNumber(number))
+plus = document.getElementById('plus');
+plus.addEventListener('click', () => holdingFunc(numArr));
 
+equals = document.getElementById('equals');
+equals.addEventListener('click', () => equalFunc(num1));
+
+clear = document.getElementById('clear');
+clear.addEventListener('click', () => acFunc(numArr,num1));
+
+function holdingFunc(numArr) {
+  num1 = parseInt(numArr.join(''));    // Storing numArr contents
+  numArr.length = 0;  // Re-emptying numArr, once contents are stored
+  console.log(num1)
+  return num1
+}
+
+function equalFunc(num1, action) {
+  action = 'plus';
+  if (action == 'plus') {
+    num2 = parseInt(numArr.join(''));
+    output = num1 + num2};
+  display.innerHTML = output;
+  acFunc();
+}
+
+function acFunc() {
+  numArr.length = 0;
+  num1= 0;
+  console.log('')
+}
+
+
+// display.addEventListener('change', () => );
+// function liveUpdate() {
+//   display
+// }
+
+console.log('-----------')
